0ebbbbd474f62b27c5df95b7fc99b2af73e10728
miro.salvagni@gmail.com <miro.salvagni@gmail.com>
2016-02-08 14:42:24 +0100

    replace in .docx works

--- src/main/java/y/ui/MainWindow.java
+++ src/main/java/y/ui/MainWindow.java
@@ -7,12 +7,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFFooter;
+import org.apache.poi.xwpf.usermodel.XWPFHeader;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 import y.yParereSRB;
 import y.em.Project;
@@ -36,6 +52,8 @@ public class MainWindow extends JFrame {
 	
 	private JPanel phase0panel;
 	
+	private Project current_project = null;
+	private XWPFDocument hdoc = null;
 	
 	public MainWindow() {
 		super("yPareriSRB - "+yParereSRB.VersionString);
@@ -73,7 +91,7 @@ public class MainWindow extends JFrame {
 		goButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				go();
+				readTemplates();
 				enablePanels();
 			}
 		});
@@ -85,108 +103,11 @@ public class MainWindow extends JFrame {
 		this.add(phase0panel, BorderLayout.NORTH);
 		
 		enablePanels();
-	//	CreateMenuBar();
 		
 		pack();
 		Utils.centerWindow(this);
 	}
-	
-//	public void CreateMenuBar()
-//	{
-//		final JMenuBar menubar = new JMenuBar();
-//		
-//		final JMenu filemenu = new JMenu("File");
-//		filemenu.setMnemonic(KeyEvent.VK_F);
-//		final JMenuItem menunew = new JMenuItem("Nuovo");
-//		menunew.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { New(); }
-//		});
-//		menunew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-//
-//		final JMenuItem openmenu = new JMenuItem("Apri...");
-//		openmenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { Open(); }
-//		});
-//		openmenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
-//
-//		final JMenuItem closemenu = new JMenuItem("Chiudi");
-//		closemenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { Close(); }
-//		});
-//		closemenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK));
-//
-//		final JMenuItem savemenu = new JMenuItem("Salva");
-//		savemenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { Save(); }
-//		});
-//		savemenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
-//
-//		final JMenuItem saveasmenu = new JMenuItem("Salva con nome...");
-//		saveasmenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { SaveAs(); }
-//		});
-//		saveasmenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
-//
-//		final JMenuItem exitmenu = new JMenuItem("Esci");
-//		exitmenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { System.exit(0); }
-//		});
-//
-//		filemenu.add(menunew);
-//		filemenu.addSeparator();
-//		filemenu.add(openmenu);
-//		filemenu.addSeparator();
-//		filemenu.add(closemenu);
-//		filemenu.addSeparator();
-//		filemenu.add(savemenu);
-//		filemenu.add(saveasmenu);
-//		filemenu.addSeparator();
-//		filemenu.add(configmenu);
-//		filemenu.addSeparator();
-//		filemenu.add(exitmenu);
-//
-//		// HELP
-//		final JMenu helpmenu = new JMenu("Help");
-//		helpmenu.setMnemonic(KeyEvent.VK_H);
-//		final JMenuItem manualmenu = new JMenuItem("Manuale");
-//		manualmenu.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { Help(); }
-//		});
-//		manualmenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
-//		helpmenu.add(manualmenu);
-//
-//		final JMenuItem aboutmenu2 = new JMenuItem("About");
-//		aboutmenu2.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent ae) { About(); }
-//		});	
-//		helpmenu.addSeparator();
-//		helpmenu.add(aboutmenu2);
-//		
-//		
-//		menubar.add(filemenu);
-//		menubar.add(Box.createHorizontalGlue());
-//		menubar.add(helpmenu);
-//
-//		setJMenuBar(menubar);
-//	}
-	
-	private Project current_project = null;
-	
-	private boolean go() {
-		try {
-			current_project = ProjectExporterProvider.importProject(yemFile.getText());
-			if (current_project == null || current_project.getSites() == null || current_project.getSites().length == 0)
-				throw new Exception("Empty project");
-			
-			return true;
-		}
-		catch (Exception e) {
-			current_project = null;
-			Utils.MessageBox("Error reading project:\n"+e.toString(), "ERROR");
-			return false;
-		}
-	}
-	
+
 	private void onClose() {
 		
 	}
@@ -195,38 +116,154 @@ public class MainWindow extends JFrame {
 		Utils.enableComponents(phase0panel, current_project == null);
 	}
 	
-
 	
-//	private void Help() {
-//		
-//	}
-//	
-//	private void About() {
-//		
-//	}
-//	
-//	private void Configure() {
-//		
-//	}
-//	
-//	private void New() {
-//		current_project = null;
-//		enablePanels();
-//	}
-//	
-//	private void Open() {
-//		
-//	}
-//	
-//	private void Close() {
-//		
-//	}
-//	
-//	private void Save() {
-//		
-//	}
-//	
-//	private void SaveAs() {
-//		
-//	}
+	
+	// phase 1
+	
+	private boolean readTemplates() {
+		try {
+			// read template
+			hdoc = new XWPFDocument(OPCPackage.open(new FileInputStream(templateFile.getText())));
+			final Set<String> fields = getTemplateFields(hdoc);
+			
+			// read project
+			current_project = ProjectExporterProvider.importProject(yemFile.getText());
+			if (current_project == null || current_project.getSites() == null || current_project.getSites().length == 0)
+				throw new Exception("Empty project");
+		
+			return true;
+		}
+		catch (Exception e) {
+			current_project = null;
+			Utils.MessageBox("Error reading project:\n"+e.toString(), "ERROR");
+			return false;
+		}
+	}
+
+	private static Set<String> getTemplateFields(XWPFDocument hdoc) {
+		final Set<String> ret = new HashSet<String>();
+
+		ret.addAll(scan(hdoc.getParagraphs()));
+
+		for (XWPFTable tbl : hdoc.getTables())
+			if (tbl != null)
+				for (XWPFTableRow row : tbl.getRows())
+					if (row != null)
+						for (XWPFTableCell cell : row.getTableCells())
+							if (cell != null)
+								ret.addAll(scan(cell.getParagraphs()));
+		
+		for (XWPFFooter footer : hdoc.getFooterList())
+			if (footer != null)
+				ret.addAll(scan(footer.getParagraphs()));
+		
+		for (XWPFHeader header : hdoc.getHeaderList())
+			if (header != null)
+				ret.addAll(scan(header.getParagraphs()));
+	
+		return ret;
+	}
+	
+	private static Set<String> scan(List<XWPFParagraph> paragraphs) {
+		final Set<String> ret = new HashSet<String>();
+		
+		for (XWPFParagraph p : paragraphs) {
+			if (p == null)
+				continue;
+			
+		    final List<XWPFRun> runs = p.getRuns();
+		    if (runs == null)
+		    	continue;
+		    
+			for (XWPFRun r : runs) {
+				if (r == null)
+					continue;
+				
+				String text = r.getText(0);
+				if (text == null || text.isEmpty())
+					continue;
+				
+				final String[] parts = text.split("\\s");
+				for (String word : parts)
+					if (word.startsWith("$"))
+						ret.add(word);
+			}
+		}
+		
+		return ret;
+	}
+	
+	
+	// phase 2
+	
+	private static boolean writeResult(XWPFDocument hdoc, HashMap<String, String> subst, String outfilename) {
+		
+		replace(hdoc.getParagraphs(), subst);
+
+		for (XWPFTable tbl : hdoc.getTables()) {
+			if (tbl == null)
+				continue;
+			
+			for (XWPFTableRow row : tbl.getRows()) {
+				if (row == null)
+					continue;
+				
+				for (XWPFTableCell cell : row.getTableCells()) {
+					if (cell == null)
+						continue;
+					
+					replace(cell.getParagraphs(), subst);
+				}
+			}
+		}
+		
+		for (XWPFFooter footer : hdoc.getFooterList()) {
+			if (footer == null)
+				continue;
+			
+			replace(footer.getParagraphs(), subst);
+		}
+		
+		for (XWPFHeader header : hdoc.getHeaderList()) {
+			if (header == null)
+				continue;
+			
+			replace(header.getParagraphs(), subst);
+		}
+		
+		try {
+			hdoc.write(new FileOutputStream(outfilename));
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private static void replace(List<XWPFParagraph> paragraphs, HashMap<String, String> subst) {
+		for (XWPFParagraph p : paragraphs) {
+			if (p == null)
+				continue;
+			
+		    final List<XWPFRun> runs = p.getRuns();
+		    if (runs == null)
+		    	continue;
+		    
+			for (XWPFRun r : runs) {
+				if (r == null)
+					continue;
+				
+				String text = r.getText(0);
+				if (text == null || text.isEmpty())
+					continue;
+				
+				for (String key : subst.keySet())
+					if (text.contains(key))
+						text = text.replace(key, subst.get(key));
+				
+				r.setText(text, 0);
+			}
+		}
+	}
 }
