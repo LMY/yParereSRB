2846e9751acc75444d987b6bae4453b170f2d3f3
miro.salvagni@gmail.com <miro.salvagni@gmail.com>
2016-03-30 12:22:54 +0200

    removed useless dependency

--- src/main/java/y/ui/MainWindow.java
+++ src/main/java/y/ui/MainWindow.java
@@ -12,7 +12,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
